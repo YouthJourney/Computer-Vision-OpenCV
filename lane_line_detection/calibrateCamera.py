b4874e55f80b46e2f0d77ef5e4263dc25ada2c8b
hong <2205365116@qq.com>
2020-11-10 16:53:54 +0800

    车道线检测

---
+++ lane_line_detection/calibrateCamera.py
@@ -0,0 +1,34 @@
+import cv2
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def pointExtractor(fname):
+    # number of boxes in the chessboard
+    objp = np.zeros((6 * 9, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+    objpoints = []
+    imgpoints = []
+
+    images = glob.glob(fname)
+
+    for idx, img in enumerate(images):
+        image = cv2.imread(img)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+        if ret == True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+    return objpoints, imgpoints
+
+
+def cameraCalibrator(objpoints, imgpoints, image):
+    imgRes = (image.shape[0], image.shape[1])
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, imgRes, None, None)
+    undistorted = cv2.undistort(image, mtx, dist, None, mtx)
+
+    return undistorted
---
+++ lane_line_detection/preprocess.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+
+def grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def warp(image):
+    w = image.shape[1]
+    h = image.shape[0]
+
+    src = np.float32([[200, 460], [1150, 460], [436, 220], [913, 220]])
+    dst = np.float32([[300, 720], [1000, 720], [400, 0], [1200, 0]])
+
+    M = cv2.getPerspectiveTransform(src, dst)
+    invM = cv2.getPerspectiveTransform(dst, src)
+
+    warped = cv2.warpPerspective(image, M, (image.shape[1], image.shape[0]), flags=cv2.INTER_LINEAR)
+
+    return warped, invM
+
+
+def threshold(image):
+    ret, image = cv2.threshold(image, 220, 225, cv2.THRESH_BINARY)
+    if not ret:
+        print('Error in thresholding')
+    else:
+        return image
